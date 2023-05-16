09d3c3e7436aac584e055851ccc80e1019fa3be9
¨mago¨ <martingramajo08@gmail.com>
2023-05-16 13:12:23 -0300

    agrego las librerias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import Home from './pages/Home';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+
 
 function App() {
   return (
-    <div>
-      <Home />
+    <div className="d-flex flex-column min-vh-100">
+      <BrowserRouter>
+        <Routes >
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
